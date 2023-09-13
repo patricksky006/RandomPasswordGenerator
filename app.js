d263e93245d40319ab9e53d2cdc623127b7e549d
patricksky006 <patrick.sky@hotmail.com>
2023-09-13 14:59:51 +0800

    fixed bug & print screen

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express')
-const { engine } = require ('express-handlebars')
+const { engine } = require('express-handlebars')
 
 const app = express()
 const port = 3000
 const generatePassword = require('./generate-password')
 
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', './views');
-app.use(express.static('public')) // 載入靜態檔案，包含Bootstrap的CSS和JS
-app.use(express.urlencoded({ extended: true })) // 使用此行來請求網址中獲取表單資料,否則就會回傳undefined的表單資料。
-
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+  res.render('index')
+})
 
-app.post('/', (req,res) => {
+app.post('/', (req, res) => {
   const options = req.body
   console.log(options)
   const password = generatePassword(options)
--- generate-password.js
+++ generate-password.js
@@ -1,60 +1,60 @@
+function generatePassword (options) {
+  // 宣告密碼是空字串
+  let password = ''
 
+  // 建立四種密碼參數的定義
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@#$%^&*()_+-=[]\{}|:";<>,./?'
 
-function generatePassword(options) {
-    //宣告密碼是空字串
-    let password = ''
+  // 根據使用者勾選設定參數進入抽選箱
+  let collection = []
 
-    //建立四種密碼參數的定義
-    const lowerCaseLetters= 'abcdefghijklmnopqrstuvwxyz'
-    const upperCaseLetters= lowerCaseLetters.toUpperCase()
-    const numbers = '1234567890'
-    const symbols = '`~!@#$%^&*()_+-=[]\{}|:";<>,./?'
-    
-
-    //根據使用者勾選設定參數進入抽選箱
-    let collection = []
-
-    if (options.lowerCase === 'on') {
-      collection = collection.concat(lowerCaseLetters.split(''))
-    }
-
-    if (options.upperCase === 'on') {
-      collection = collection.concat(upperCaseLetters.split(''))
-    }
-
-    if (options.numbers === 'on') {
-      collection = collection.concat(numbers.split(''))
-    }
-
-    if (options.symbols === 'on') {
-      collection = collection.concat(symbols.split(''))
-    }
-    
-    if (options.excludedCharacters) {
-      collection = collection.filter((character) => {
-        return !options.excludedCharacters.includes(character)
-      })
-    }
-    
-    //限制條件
-    if (options.length < 4 || options.length > 16 || options.passwordLength === '') {
-    return password = 'Please enter the correct password length'
+  if (options.lowerCase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
   }
 
-    if (options.lowerCase !== 'on' && options.upperCase !== 'on' && options.numbers !== 'on' && options.symbols !== 'on') {
-      return password = 'Please select at least one option'
-    }
-
-    //根據使用者需求抽多少個密碼
-      
-      for(let i = 1; i <= options.passwordLength; i++) {
-        let randomIndex = Math.floor(Math.random() * collection.length)
-        password += collection[randomIndex]
-      }
-      console.log("password", password)
-      return password
-      
+  if (options.upperCase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
   }
 
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
 
-  module.exports = generatePassword
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  if (options.excludedCharacters) {
+    collection = collection.filter((character) => {
+      return !options.excludedCharacters.includes(character)
+    })
+  }
+
+  // 處理錯誤條件
+  if (options.length < 4 || options.length > 16 || options.passwordLength === '') {
+    password = 'Please enter the correct password length.'
+    return password
+  }
+
+  if (options.lowerCase !== 'on' && options.upperCase !== 'on' && options.numbers !== 'on' && options.symbols !== 'on') {
+    password = 'Please select at least one option.'
+    return password
+  }
+
+  if (collection.length === 0) {
+    password = 'Please enter the valid password length/options.'
+    return password
+  }
+
+  // 根據使用者需求抽密碼
+  for (let i = 1; i <= options.passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * collection.length)
+    password += collection[randomIndex]
+  }
+  return password
+}
+
+module.exports = generatePassword
